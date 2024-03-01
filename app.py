98449c81c97f460be2cf5ba6fcdb578fb76c7e1f
goetzprtnrs <Andreas.Koch@goetzpartners.com>
2024-03-01 20:16:49 +0100

    fix paths github

--- app.py
+++ app.py
@@ -26,20 +26,20 @@ server = app.server
 
 # Beispiel-Datensätze einlesen
 df_weichen = pd.read_excel('https://raw.githubusercontent.com/goetzprtnrs/infdb/main/data/2022_2105-V_Stueckliste_Weichen.xlsx')
-df_bruecken = pd.read_excel(r'.\data\2022_2106-V_Stueckliste_Bruecken.xlsx')
-df_tunnel = pd.read_excel(r'.\data\2022_2108-V_Stueckliste_Tunnel.xlsx')
-df_stuetzbauwerke = pd.read_excel(r'.\data\2022_2110-V_Stueckliste_Stuetzbauwerke_Bauwerksklasse(BWK)3.xlsx')
-df_schallschutzwande = pd.read_excel(r'.\data\2022_2111-V_Stueckliste_Schallschutzwaende.xlsx')
-df_bahnubergange = pd.read_excel(r'.\data\2022_2112-V_Stueckliste_Bahnuebergaenge.xlsx')
+df_bruecken = pd.read_excel('https://raw.githubusercontent.com/goetzprtnrs/infdb/main/data/2022_2106-V_Stueckliste_Bruecken.xlsx')
+df_tunnel = pd.read_excel('https://raw.githubusercontent.com/goetzprtnrs/infdb/main/data/2022_2108-V_Stueckliste_Tunnel.xlsx')
+df_stuetzbauwerke = pd.read_excel('https://raw.githubusercontent.com/goetzprtnrs/infdb/main/data/2022_2110-V_Stueckliste_Stuetzbauwerke_Bauwerksklasse(BWK)3.xlsx')
+df_schallschutzwande = pd.read_excel('https://raw.githubusercontent.com/goetzprtnrs/infdb/main/data/2022_2111-V_Stueckliste_Schallschutzwaende.xlsx')
+df_bahnubergange = pd.read_excel('https://raw.githubusercontent.com/goetzprtnrs/infdb/main/data/2022_2112-V_Stueckliste_Bahnuebergaenge.xlsx')
 df_GSL = pd.read_excel('https://raw.githubusercontent.com/goetzprtnrs/infdb/main/data/2022_2101-V_Gesamtstreckenliste(GSL).xlsx')
-df_ETCS = pd.read_excel(r'.\data\ETCS_heute_und_geplant.xlsx')
-df_Traffic = pd.read_excel(r'.\data\StrNr_Average_Anual_Traffic_Flow.xlsx')
-df_HLK_Zeitraum = pd.read_excel(r'.\data\StrNr_HLK_Zeitraum.xlsx')
+df_ETCS = pd.read_excel('https://raw.githubusercontent.com/goetzprtnrs/infdb/main/data/ETCS_heute_und_geplant.xlsx')
+df_Traffic = pd.read_excel('https://raw.githubusercontent.com/goetzprtnrs/infdb/main/data/StrNr_Average_Anual_Traffic_Flow.xlsx')
+df_HLK_Zeitraum = pd.read_excel('https://raw.githubusercontent.com/goetzprtnrs/infdb/main/data/StrNr_HLK_Zeitraum.xlsx')
 
 # Pfad zu den Excel-Dateien
-file_path_bu = r'.\data\20240229_Matching_2112-V_Stueckliste_Bahnuebergaenge_vClean.xlsx'
-file_path_br = r'.\data\20240229_Matching_Brücken_vClean.xlsx'
-file_path_tu = r'.\data\20240229_Matching_Tunnel_vClean.xlsx'
+file_path_bu = 'https://raw.githubusercontent.com/goetzprtnrs/infdb/main/data/20240229_Matching_2112-V_Stueckliste_Bahnuebergaenge_vClean.xlsx'
+file_path_br = 'https://raw.githubusercontent.com/goetzprtnrs/infdb/main/data/20240229_Matching_Brücken_vClean.xlsx'
+file_path_tu = 'https://raw.githubusercontent.com/goetzprtnrs/infdb/main/data/20240229_Matching_Tunnel_vClean.xlsx'
 
 
 df_bu = pd.read_excel(file_path_bu)
